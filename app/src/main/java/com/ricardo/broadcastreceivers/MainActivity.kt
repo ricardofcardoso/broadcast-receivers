85340af9deb0cae8f69449a9703fa259b09072b7
Ricardo Cardoso <riques.fcardoso@gmail.com>
2025-03-29 23:41:51 +0000

    Adjust MainActivity UI

--- app/src/main/java/com/ricardo/broadcastreceivers/MainActivity.kt
+++ app/src/main/java/com/ricardo/broadcastreceivers/MainActivity.kt
@@ -6,13 +6,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.ricardo.broadcastreceivers.receivers.AirplaneModeReceiver
 import com.ricardo.broadcastreceivers.receivers.BluetoothReceiver
 import com.ricardo.broadcastreceivers.ui.theme.BroadcastreceiversTheme
@@ -42,10 +44,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             BroadcastreceiversTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                    ) {
+                        Text(
+                            text = "Broadcast Receivers Manager",
+                            style = MaterialTheme.typography.headlineMedium
+                        )
+                    }
                 }
             }
         }
@@ -54,21 +64,5 @@ class MainActivity : ComponentActivity() {
     override fun onDestroy() {
         super.onDestroy()
         broadcastManager.unregisterAll()
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BroadcastreceiversTheme {
-        Greeting("Android")
     }
 }
